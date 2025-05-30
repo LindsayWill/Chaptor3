80f944c6128a2fc6bd5d3330566005f583a8e9c3
LindsayWill <109075568+LindsayWill@users.noreply.github.com>
2025-05-29 22:13:51 -0500

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿#region Exploring unary operators
+
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+#endregion
+
+#region Exploring unary operators
+
+int c = 3;
+int d = ++c;
+WriteLine($"c is {c}, d is {d}");
+
+#endregion
+
+//Exploring Binary aritmetic operators.
