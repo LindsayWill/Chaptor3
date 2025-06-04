4b3c191c4b57886ca8682913e59584be3eb4cb52
LindsayWill <109075568+LindsayWill@users.noreply.github.com>
2025-06-03 21:26:50 -0500

    6.3.25 9.26

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -39,9 +39,57 @@ WriteLine($"g / f = {g / f}");
 //Returns floating-point due to floating point operator g.
 
 //Assingment operator
-int p = 6;
-p += 3; //Equivalent to: p = p + 3;
-p -= 3; // Equivalent to: p = p - 3;
-p *= 3; // Equivalent to: p = p * 3;
-p /= 3; // Equivalent to: p = p / 3;
+//int p = 6;
+//p += 3; //Equivalent to: p = p + 3;
+//p -= 3; // Equivalent to: p = p - 3;
+//p *= 3; // Equivalent to: p = p * 3;
+//p /= 3; // Equivalent to: p = p / 3;
 
+//Boolean statements p and q
+//with values of true or false with truth tables output with AND OR and XOR logical operators.
+bool p = true;
+bool q = false;
+WriteLine($"AND   | p    | q    ");
+WriteLine($"p     | {p & p,-5} | {p & q,-5} ");
+WriteLine($"q     | {q & p,-5} | {q & q,-5} ");
+WriteLine();
+WriteLine($"OR   | p    | q    ");
+WriteLine($"p     | {p | p,-5} | {p | q,-5} ");
+WriteLine($"q     | {q | p,-5} | {q | q,-5} ");
+WriteLine();
+WriteLine($"XOR   | p    | q    ");
+WriteLine($"p     | {p ^ p,-5} | {p ^ q,-5} ");
+WriteLine($"q     | {q ^ p,-5} | {q ^ q,-5} ");
+
+//Declare a function that writes message to console and returns true.
+
+static bool DoStuff()
+{
+    WriteLine("I am doing some stuff");
+    return true;
+}
+WriteLine();
+// Note that DoStuff() returns true.
+WriteLine($"p & DoStuff() = {p & DoStuff()}");
+WriteLine($"q & DoStuff() = {q & DoStuff()}");
+
+WriteLine();
+// Note that DoStuff() returns true.
+WriteLine($"p && DoStuff() = {p && DoStuff()}");
+WriteLine($"q && DoStuff() = {q && DoStuff()}");
+
+//Declare 2 integer variables named x and y with vlaues 10 and 6.
+//Output results applying AND, OR, and XOR bitwise operators.
+
+WriteLine();
+
+int x = 10;
+int y = 6;
+
+WriteLine($"Expression | Decimal |  Binary");
+WriteLine($"------------------------------");
+WriteLine($"x        | {x,7} | {x:B8}");
+WriteLine($"x        | {y,7} | {y:B8}");
+WriteLine($"x & y    | {x & y,7} | {x & y:B8}");
+WriteLine($"x | y    | {x | y,7} | {x | y:B8}");
+WriteLine($"x ^ y    | {x ^ y,7} | {x ^ y:B8}");
