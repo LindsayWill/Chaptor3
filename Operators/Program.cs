3c6336430d286642c822a545f9d0f7459d34bc44
LindsayWill <109075568+LindsayWill@users.noreply.github.com>
2025-06-03 20:34:00 -0500

    6.3.25

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -15,3 +15,33 @@ WriteLine($"c is {c}, d is {d}");
 #endregion
 
 //Exploring Binary aritmetic operators.
+/*
+ * Declaring and Assigning 2 integaer variables named e and f 
+ * and applying the five common binary arithmetic operators to two numbers.
+ * */
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+/*Declaring and assigning value to double variable name g
+ * to show difference between whole number and real number divisions.*/
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
+//Returns floating-point due to floating point operator g.
+
+//Assingment operator
+int p = 6;
+p += 3; //Equivalent to: p = p + 3;
+p -= 3; // Equivalent to: p = p - 3;
+p *= 3; // Equivalent to: p = p * 3;
+p /= 3; // Equivalent to: p = p / 3;
+
